52a6d8fb67707243e505c8facdcdaf5ce97612ed
zurdomagic <danielmataalv006@gmail.com>
2026-05-15 19:00:53 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuinielaGuaymura.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
